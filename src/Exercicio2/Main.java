4d5810fdaedca204cb929fa1bfaf0655512afe55
Renato Vianna <marquez.vianna@gmail.com>
2022-05-11 20:55:28 -0300

    firts commit

---
+++ src/Exercicio2/Main.java
@@ -0,0 +1,15 @@
+package Exercicio2;
+
+public class Main {
+
+public static void main(String[] args) {
+
+        //Quadrilatero
+        System.out.println("Exercício 2");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,5,9);
+        Quadrilatero.area(5f,5f);
+        }
+
+ }
